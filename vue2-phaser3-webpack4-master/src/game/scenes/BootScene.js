e37652fe0b7defdadb53d9fe45206692b6b27d7e
Oriol Perarnau Arnau <oriol.perarnau.arnau@gmail.com>
2019-04-01 18:34:56 +0200

    intereccio amb carta

--- vue2-phaser3-webpack4-master/src/game/scenes/BootScene.js
+++ vue2-phaser3-webpack4-master/src/game/scenes/BootScene.js
@@ -2,6 +2,7 @@ import {Scene} from 'phaser'
 import sky from '@/game/assets/sky.png';
 import bomb from '@/game/assets/bomb.png';
 import tauler from '@/game/assets/tauler.png';
+import carta_base from '@/game/assets/card_base.png';
 
 export default class BootScene extends Scene {
     constructor() {
@@ -12,6 +13,7 @@ export default class BootScene extends Scene {
         this.load.image('sky', sky)
         this.load.image('bomb', bomb)
         this.load.image('tauler', tauler)
+        this.load.image('carta', carta_base)
         // this.load.audio('thud', ['assets/thud.mp3', 'assets/thud.ogg'])
     }
 
--- vue2-phaser3-webpack4-master/src/game/scenes/FightScene.js
+++ vue2-phaser3-webpack4-master/src/game/scenes/FightScene.js
@@ -1,26 +1,88 @@
 import { Scene } from 'phaser';
 
+var graphics;
+var rect;
+var hitBoxTauler;
+var point;
+var zone;
+
 export default class FightScene extends Scene {
   constructor () {
     super({ key: 'FightScene' });
+
+  }
+
+  preload() {
   }
 
   create () {
-    console.log("Starting FightScene ...");
-    let i = this.add.image(200, 200, 'tauler')
-    i.scaleX = 3
-    i.scaleY = 3
-    console.log(i);
+    //Zones
+    graphics = this.add.graphics({ lineStyle: { width: 2, color: 0x0000aa }, fillStyle: { color: 0xaa0000 }});
+    rect = new Phaser.Geom.Rectangle(200, 500, 400, 100);
+    point = new Phaser.Geom.Circle(200, 500, 10);
 
-    const bomb = this.physics.add.image(400, 200, 'bomb');
-    bomb.scaleX = 2;
-    bomb.scaleY = 2;
-    bomb.setCollideWorldBounds(true);
-    bomb.body.onWorldBounds = true; // enable worldbounds collision event
-    bomb.setBounce(1);
-    bomb.setVelocity(200, 20);
+    //Tauler
+    this.tauler = this.add.image(200, 200, 'tauler');
+    this.tauler.scaleX = 3
+    this.tauler.scaleY = 3
+    hitBoxTauler = new Phaser.Geom.Rectangle(56, 56, 289, 289);
+    zone = this.add.zone(200, 200).setRectangleDropZone(289, 289);
+
+
+    //Cartes
+    this.carta = this.add.sprite(400, 500, 'carta').setInteractive({ draggable: true });
+    this.carta.originX = 100;   //BUSCAR AIXO
+    this.carta.originY = 200;   //BUSCAR AIXO
+    //this.input.setDraggable(this.carta);
+
+   graphics.lineStyle(2, 0xffff00);
+
+   this.input.on('dragstart', function (pointer, gameObject) {
+
+       this.children.bringToTop(gameObject);
+
+   }, this);
+
+   this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+
+       gameObject.x = dragX;
+       gameObject.y = dragY;
+
+   });
+
+   this.input.on('dragenter', function (pointer, gameObject, dropZone) {
+
+   });
+
+   this.input.on('dragleave', function (pointer, gameObject, dropZone) {
+
+   });
+
+   this.input.on('drop', function (pointer, gameObject, dropZone) {
+
+       gameObject.input.enabled = false;
+
+   });
+
+   this.input.on('dragend', function (pointer, gameObject, dropped) {
+
+       if (!dropped)
+       {
+           gameObject.x = gameObject.input.dragStartX;
+           gameObject.y = gameObject.input.dragStartY;
+       }
+   });
   }
 
   update () {
+
+    point.x = this.carta.x;
+    point.y = this.carta.y;
+
+    graphics.clear();
+    graphics.strokeRectShape(rect);
+    graphics.fillCircleShape(point);
+    graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
+
   }
 }
