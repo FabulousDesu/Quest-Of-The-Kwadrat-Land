18278bc86475ce33d1640cf5c96de3972d51c21c
Oriol Perarnau Arnau <oriol.perarnau.arnau@gmail.com>
2019-05-09 17:36:57 +0200

    cartacarta

--- vue2-phaser3-webpack4-master/src/game/scenes/Card.js
+++ vue2-phaser3-webpack4-master/src/game/scenes/Card.js
@@ -1,35 +1,58 @@
 export class Carta extends Phaser.GameObjects.Sprite{
-  constructor(scene, x, y){
+  constructor(scene, x, y, type, forma = [[0,0,0,0],[0,0,0,0],[0,0,1,0],[0,0,0,0]]){
     super(scene, x, y, 'carta');
 
     //Dibuixar la Peca sobre la Carta
-    this.setDepth(0)
-    this.val = [[0,1,0,0],
-                [0,1,1,0],
-                [0,1,1,0],
-                [0,0,1,0]]; // = random figure with val valors
-    this.type = 0;
+    this.setDepth(0);
+    this.val = forma; // = random figure with val valors
+    this.type = type;          // 1= foc; 2 = gel 3= veri 4= extra
     this.fitxa = [];
+    this.inicialPos = [x,y];
 
-    for(var i = -2; i < 2; i++){
-      for(var j = -2; j < 2; j++){
-        if(this.val[j+2][i+2] == 1){
-          this.fitxa.push(scene.add.sprite(x+16*i,y+16*j,'fitxa', 3));
-          this.fitxa[this.fitxa.length-1].setScale(0.5);
-          this.fitxa[this.fitxa.length-1].setDepth(1);
+    const midaFitxa = 32;
+
+    var that = this;
+    this.dibuixarPeces = function(escala, marcada = false){
+      that.fitxa.forEach(function(element){element.destroy()});
+      let offset = midaFitxa * escala;
+      let nova_x = that.x
+      let nova_y = that.y;
+      if (marcada){
+        nova_x = scene.input.mousePointer.x - offset/2;
+        nova_y = scene.input.mousePointer.y - offset/2;
+      }
+
+      for(let i = -2; i < 2; i++){
+        for(let j = -2; j < 2; j++){
+          if(that.val[j+2][i+2] == 1){
+            that.fitxa.push(scene.add.sprite(nova_x+offset/2+offset*i,nova_y+offset/2+offset*j,'fitxa', that.type-1));
+            that.fitxa[this.fitxa.length-1].setScale(escala);
+            that.fitxa[this.fitxa.length-1].setDepth(1);
+          }
         }
       }
     }
+
+    this.dibuixarPeces(0.5);
 
     //Fer que sigui setDraggable
     this.setInteractive();
     scene.input.setDraggable(this);
 
     this.lastPos = [this.x, this.y];
+    this.on('pointerover', function () {
+        that.setScale(1.5);
+        that.dibuixarPeces(0.75);
+    });
 
-    var that = this;
+    this.on('pointerout', function () {
+      this.setScale(1);
+      that.dibuixarPeces(0.5);
+    });
+
     this.on('dragstart', function (pointer) {
-      that.setTint(0xff0000);
+      that.setVisible(false);
+      that.dibuixarPeces(1.6, true);
     });
 
     this.on('drag', function (pointer, dragX, dragY) {
@@ -42,11 +65,25 @@ export class Carta extends Phaser.GameObjects.Sprite{
         element.x -= that.lastPos[0] - that.x;
         element.y -= that.lastPos[1] - that.y;
       })
-      
+
     });
 
     this.on('dragend', function (pointer) {
-      that.clearTint();
+      that.setVisible(true);
+      that.dibuixarPeces(0.5);
+      if(that.scene.tauler.colocarCarta(that)){
+        that.fitxa.forEach(function(element){element.destroy()});
+        that.destroy();
+      }else{
+        that.x = that.inicialPos[0];
+        that.y = that.inicialPos[1];
+        that.dibuixarPeces(0.5);
+      }
     });
+
+    //EXTRES
+    this.getMatriuPeca = function(){
+      return this.val;
+    }
   }
 }
--- vue2-phaser3-webpack4-master/src/game/scenes/FightScene.js
+++ vue2-phaser3-webpack4-master/src/game/scenes/FightScene.js
@@ -16,16 +16,14 @@ export default class FightScene extends Scene {
     rect = new Phaser.Geom.Rectangle(200, 500, 400, 100);
 
     //Tauler
-    this.tauler = this.add.image(200, 200, 'tauler');
-    this.tauler.scaleX = 3;
-    this.tauler.scaleY = 3;
+    this.tauler2 = this.add.image(200, 200, 'tauler');
+    this.tauler2.setScale(3);
 
-    this.children.add(new Tauler(this, 200, 200));
-    this.children.add(new Carta(this, 400, 400));
-    this.children.add(new Peca(this, 350, 500, 1, "fire"));
+    this.tauler = new Tauler(this, 200, 200);
+    this.children.add(this.tauler);
+    this.children.add(new Carta(this, 400, 400, 2, [[0,0,0,0],[0,1,1,0],[0,1,1,0],[0,1,1,0]]));
+    this.children.add(new Carta(this, 500, 400, 1, [[0,1,0,0],[0,0,1,0],[0,0,1,0],[0,0,1,0]]));
     this.children.add(new Enemy(this, 600, 200));
-
-    //zone = this.add.zone(200, 200, 300, 300).setRectangleDropZone(300, 300);
   }
 
   update () {
@@ -47,6 +45,8 @@ export class Tauler extends Phaser.GameObjects.Grid{
   constructor (scene, x, y){
     super(scene, x, y, 300, 300, 50, 50, 0x00b9f2).setAltFillStyle(0x016fce);
     this.mida = 6;
+
+    this.fitxesTauler = [];
     this.matriu = [[0,0,0,0,0,0],
                    [0,0,0,0,0,0],
                    [0,0,0,0,0,0],
@@ -56,54 +56,66 @@ export class Tauler extends Phaser.GameObjects.Grid{
     //Buida = 0, Foc = 1, Gel = 2, Veri = 3 i Extra = 4
 
     console.log(this.matriu);
-    this.valors = {"foc": 0, "gel": 0, "veri": 0, "extra": 0}
-    }
+    this.valors = [0,0,0,0,0];
 
-}
+    this.scene = scene;
 
-export class Peca extends Phaser.GameObjects.Sprite{
-  constructor(scene, x, y, val, type){
-    var a = super(scene, x, y, 'carta');
-    this.val = [[0,0,0,0],
-                [0,1,1,0],
-                [0,1,1,0],
-                [0,0,0,0]]; // = random figure with val valors
-    this.type = type;
-    this.fitxa = [];
+    let that = this;
 
-    for(var i = -2; i < 2; i++){
-      for(var j = -2; j < 2; j++){
-        if(this.val[j+2][i+2] == 1){
-          this.fitxa.push(scene.add.sprite(x+16*i,y+16*j,'fitxa', 3));
-          this.fitxa[this.fitxa.length-1].scaleX = 0.5
-          this.fitxa[this.fitxa.length-1].scaleY = 0.5
+    //Funcio per colocar carta, retorna cert si l'ha pogut colocar
+    this.colocarCarta = function(carta){
+      //PREGUNTAR VECTOR
+      let mousePos = [that.scene.input.mousePointer.x, that.scene.input.mousePointer.y]
+
+      if ( ! (mousePos[0] >= that.x - 150 && mousePos[0] <= that.x + 150 && mousePos[1] >= that.y - 150 && mousePos[1] <= that.y + 150)){
+        return false;
+      }
+
+      let nou_mouse = [mousePos[0] - (that.x - 150), mousePos[1] - (that.y - 150)]
+      let casellaSeleccionada = [Math.trunc(nou_mouse[0]/50), Math.trunc(nou_mouse[1]/50)];
+
+      //Comprobar que cap
+      for (let i = -2; i < 2; i++){
+        for (let j = -2; j < 2; j++){
+          //Existeix la peca
+          if (carta.val[j+2][i+2] != 0){
+
+            //La peca cap dintre els bordes
+            if (casellaSeleccionada[0] + i < 0 || casellaSeleccionada[0] + i >= 6 || casellaSeleccionada[1] + j < 0 || casellaSeleccionada[1] + j >= 6){
+              return false;
+            }
+
+            //La pos no esta ocupada
+            if (that.matriu[casellaSeleccionada[1] + j][casellaSeleccionada[0] + i] != 0){
+              return false;
+            }
+          }
         }
       }
 
+      //Colocar Peca
+      let aux = 0;
+      for (let i = -2; i < 2; i++){
+        for (let j = -2; j < 2; j++){
+          if (carta.val[j+2][i+2] != 0){
+            aux++;
+            that.matriu[casellaSeleccionada[1] + j][casellaSeleccionada[0] + i] = carta.type;
+            let sprite_aux = that.scene.add.sprite((casellaSeleccionada[0] + i)*50 + (that.x - 150 + 25) , (casellaSeleccionada[1] + j)*50 + (that.x - 150 + 25), 'fitxa', carta.type -1);
+            sprite_aux.setScale(1.6);
+            that.fitxesTauler.push(sprite_aux);
+          }
+        }
+      }
+      that.valors[carta.type] += aux;
+      console.log(that.matriu);
+
+      return true;
     }
 
-    this.punt_clau = [2,2];
-    this.setInteractive();
-    scene.input.setDraggable(this);
+    this.buidarTauler = function(){
+      that.fitxesTauler.forEach(function(element){element.destroy()});
+      that.fitxesTauler = [];
 
-    var that = this;
-    this.on('dragstart', function (pointer) {
-
-      that.setTint(0xff0000);
-
-    });
-
-    this.on('drag', function (pointer, dragX, dragY) {
-
-      that.x = dragX;
-      that.y = dragY;
-
-    });
-
-    this.on('dragend', function (pointer) {
-
-      that.clearTint();
-
-    });
+    }
   }
 }
